277a68fbeb590dd8e466baca0f370223efe0e1af
B4ZE\wiaph <wi.aphikhun_st@tni.ac.th>
2023-03-11 20:30:16 +0700

    <Add> Box Spawn

--- Assets/Scripts/BoxSpawner.cs
+++ Assets/Scripts/BoxSpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BoxSpawner : MonoBehaviour
@@ -9,17 +10,24 @@ public class BoxSpawner : MonoBehaviour
 
     [SerializeField] private Transform[] spawnPoints;
 
-    private GameObject[] boxs;
+    private List<GameObject> boxs = new();
+    private List<GameObject> randomBoxs = new();
     // Start is called before the first frame update
     void Awake()
     {
-        spawnPoints = GetComponentsInChildren<Transform>();
-
-
-
-        for(int i = 0; i < spawnPoints.Length; i++)
+        for(int i = 0; i < 4; i++)
         {
-            
+            boxs.Add(normalBox);
+        }
+        boxs.Add(specialBox);
+
+        randomBoxs = boxs.OrderBy(x => Random.value).ToList();
+
+        int j = 0;
+        foreach(var box in randomBoxs)
+        {
+            Instantiate(box, spawnPoints[j]);
+            j++;
         }
     }
 }
